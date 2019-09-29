a2becc0c22ee0f845cb367db81c7441987296822
RobertRisler <roberto.lillo@usach.cl>
2019-09-29 20:39:32 -0300

    CHG: Se mejora resultados por pantall

--- src/pipeline/guardar.c
+++ src/pipeline/guardar.c
@@ -6,8 +6,11 @@ int main(int argc, char *argv[])
     // Cantidad de imagenes a procesar
     int cantImagenes = atoi(argv[1]);
 
+    printf("Resultado del umbral de negro.\nEsta la imagen por sobre el umbral?\n");
+
     int img = 1;
-    char nombre[15];
+    char nombreSalida[15];
+    char nombreIMG[15];
     while (img <= cantImagenes)
     {
         int resultadoUmbral;
@@ -19,9 +22,9 @@ int main(int argc, char *argv[])
         int matriz[dimensiones[0]][dimensiones[1]];
         read(STDIN_FILENO, matriz, dimensiones[0] * dimensiones[1] * sizeof(int));
 
-        sprintf(nombre, "salida%i.ppm", img);
+        sprintf(nombreSalida, "salida%i.png", img);
 
-        FILE *nuevaIMG = fopen(nombre, "wb");
+        FILE *nuevaIMG = fopen(nombreSalida, "wb");
         fprintf(nuevaIMG, "P6\n%i %i 255\n", dimensiones[1], dimensiones[0]);
         int i, j;
         for (i = 0; i < dimensiones[0]; i++)
@@ -37,8 +40,8 @@ int main(int argc, char *argv[])
 
         if (argv[2] != NULL)
         {
-
-            printf("Imagen\t->\t");
+            sprintf(nombreIMG, "imagen %d", img);
+            printf("%s\t->\t", nombreIMG);
             if (resultadoUmbral == 0)
             {
                 printf("no\n");
